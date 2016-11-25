e73905b94310dc280a2034b83a9cca69cd44c22c
Ashhar Hasan <hashhar_dev@outlook.com>
2016-11-25 10:58:41 +0530

    Working client +server

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace WebApiClient
+{
+	class WebApiClient
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Starting client...");
+
+			int port = 1234;
+			TcpClient client = new TcpClient("localhost", port);
+			NetworkStream stream = client.GetStream();
+			StreamReader reader = new StreamReader(stream);
+			StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+
+			while (true)
+			{
+				Console.Write("Enter to send: ");
+				string lineToSend = Console.ReadLine();
+				Console.WriteLine("Sending to server: " + lineToSend);
+				writer.WriteLine(lineToSend);
+				string lineReceived = reader.ReadLine();
+				Console.WriteLine("Received from server: " + lineReceived);
+			}
+		}
+	}
+}
---
+++ WebApiServer/Program.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using EditCorrector;
+using FeatureExtractor;
+using FeatureExtractor.Abstract;
+
+namespace WebApiServer
+{
+	public class dataModel
+	{
+		public string Sentence { get; set; }
+		public Dictionary<int, string> Corrections { get; set; }
+	}
+
+	public class WebApiServer
+	{
+		private static readonly string SolutionPath = ContextSensitiveSpellingCorrection.ContextSensitiveSpellingCorrection.SolutionPath;
+		private static ContextSensitiveSpellingCorrection.ContextSensitiveSpellingCorrection contextSensitiveSpellingCorrection;
+
+		public static void Main()
+		{
+			SetupService();
+			Console.WriteLine("Starting echo server...");
+
+			int port = 1234;
+			TcpListener listener = new TcpListener(IPAddress.Loopback, port);
+			listener.Start();
+
+			TcpClient client = listener.AcceptTcpClient();
+			NetworkStream stream = client.GetStream();
+			StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+			StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+
+			while (true)
+			{
+				string inputLine = "";
+				while (inputLine != null)
+				{
+					inputLine = reader.ReadLine();
+					var testSentence = inputLine;
+					Dictionary<int, string> wordsList = contextSensitiveSpellingCorrection.Predict(testSentence);
+					var outLine = "";
+					var jsonObj = new dataModel();
+					jsonObj.Sentence = inputLine;
+					jsonObj.Corrections = wordsList;
+					var _data = new List<dataModel>();
+					_data.Add(jsonObj);
+					string json = JsonConvert.SerializeObject(_data.ToArray());
+					writer.WriteLine("Corrections: " + json);
+					Console.WriteLine("Corrections: " + json);
+				}
+				Console.WriteLine("Server saw disconnect from client.");
+			}
+		}
+
+		private static string ReturnNWord(int n, string str)
+		{
+			var words = str.Split(' ');
+			return words[n];
+		}
+
+		private static void SetupService()
+		{
+			Console.WriteLine("Starting application. It may take some time to load dictionaries.");
+			var editCorrector = new Spelling();
+			var confusionSets = GetConfusionSets();
+			var trainingCorpora = GetCorpora();
+			IPosTagger posTagger = new PosTagger();
+			contextSensitiveSpellingCorrection = new ContextSensitiveSpellingCorrection.ContextSensitiveSpellingCorrection(posTagger, trainingCorpora, confusionSets, false);
+		}
+
+		private static IEnumerable<string[]> GetConfusionSets()
+		{
+			return new List<string[]>
+			{
+				new[] {"peace", "piece"},
+				new[] {"where", "were"},
+				new[] {"hour", "our"},
+				new[] {"by", "buy", "bye"},
+				new[] {"cite", "site", "sight"},
+				new[] {"coarse", "course"},
+				new[] {"desert", "dessert"},
+				new[] {"knew", "new"},
+				new[] {"hear", "here"},
+				new[] {"vain", "vane", "vein"},
+				new[] {"loose", "lose"},
+				new[] {"plaine", "plane"},
+				new[] {"principal", "principle"},
+				new[] {"sea", "see"},
+				new[] {"quiet", "quit", "quite"},
+				new[] {"rain", "reign", "rein"},
+				new[] {"waist", "waste"},
+				new[] {"weak", "week"},
+				new[] {"weather", "whether"},
+				new[] {"fourth", "forth"},
+				new[] {"passed", "past"},
+				new[] {"council", "counsel"},
+				new[] {"complement", "compliment"},
+				new[] {"their", "there"},
+				new[] {"later", "latter"},
+				new[] {"threw", "through"},
+				new[] {"to", "too", "two"},
+				new[] {"brake", "break"}
+			};
+		}
+
+		private static IEnumerable<string> GetCorpora()
+		{
+			var corpus = File.ReadAllText(Path.Combine(SolutionPath, @"Corpus\Release Corpus.txt"));
+			return corpus.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+		}
+	}
+}
